c15949f8a01e341d4a2761b803a9457e92895ed1
Matthew Chau <26354088+TOLOTOLO999@users.noreply.github.com>
2020-06-03 08:57:59 +0800

    update project structure

---
+++ hw/final_project/src/Data.py
@@ -0,0 +1,38 @@
+# get data from stock market
+
+import time as Time
+from datetime import datetime, timedelta
+
+import matplotlib.pyplot as plt
+from iexfinance.stocks import Stock
+
+a = Stock("AAPL", token="sk_434806dae94a4ee69daa8375f33da6f5")  # token is the secret API key for our account
+
+time_offset = timedelta(hours=3)
+nyc_time = datetime.now() + time_offset
+
+intraday_prices = []
+time = []
+# counter = 0
+
+# while counter < 5:
+while (nyc_time.hour > 9) and (nyc_time.minute > 30):
+    intraday_prices.append(a.get_price())
+
+    time.append(str(nyc_time.hour) + str(":") + str(nyc_time.minute))
+
+    Time.sleep(60)
+    nyc_time = datetime.now() + time_offset
+
+    if (nyc_time.hour > 14) and (nyc_time.minute > 30) and (nyc_time.second > 0):
+        break
+    # counter +=1
+
+fig, ax = plt.subplots()
+ax.plot(time, intraday_prices, color="green")
+
+ax.set(xlabel='Time', ylabel='Price',
+       title='Apple Intraday Prices')
+ax.grid()
+
+plt.show()
