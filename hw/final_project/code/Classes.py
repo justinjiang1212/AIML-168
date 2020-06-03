df20c4ff1f7adcca0cee20dbc124b8189e79751a
justinjiang1212 <jbjiang@hmc.edu>
2020-06-02 17:11:24 -0700

    added update data values

--- hw/final_project/code/Classes.py
+++ hw/final_project/code/Classes.py
@@ -1,25 +1,54 @@
 #classes for trading bot
 
 from enum import Enum
-import Data
-from yahoo_fin import stock_info as si
+#import Data
+#from yahoo_fin import stock_info as si
+from datetime import datetime, timedelta, date
+from iexfinance.stocks import Stock
+from iexfinance.stocks import get_historical_intraday
+import pandas as pd
+import os
 
-class EquityType(Enum):
+IEX_TOKEN='sk_434806dae94a4ee69daa8375f33da6f5'
+time_offset = timedelta(hours = 3)
+
+'''class EquityType(Enum):
   STOCK = 1
   FUTURE = 2
   OPTION = 3
-  BOND = 4
+  BOND = 4'''
+
+def daterange(start_date, end_date):
+  for n in range(int ((end_date - start_date).days)):
+    yield start_date + timedelta(n)
+
 
 class Equity:
   def __init__(self, name=None, ticker = None):
-        self.name = name                  #String, colloquial name of equity 
-        self.ticker = ticker              #String, ticker name of equity
-        self.dayData = None               #Data Frame, contains trading data starting from 12:00 am EST of the current day
-        self.historicalData = None        #dictionary of data frames of Day, with the key being a date in format MM/DD/YYYY and the value being a data frame of that day's trading data
+    self.name = name                                                 #String, colloquial name of equity 
+    self.ticker = ticker                                             #String, ticker name of equity
+    self.dayData = pd.DataFrame(columns=['DateTime','Price'])        #Data Frame, contains trading data starting from 12:00 am EST of the current day
+    self.historicalData = {}                                          #dictionary of data frames of Day, with the key being a date in format MM/DD/YYYY and the value being a data frame of that day's trading data
+    self.iexVar = Stock(ticker, token = IEX_TOKEN)
 
 
-        def download_dayData(ticker, startDate, endDate):
-          data = si.get_data(ticker, start_date = startDate, end_date = endDate)
+  def updateLiveData(self):
+    length = len(self.dayData)
+    self.dayData.loc[length + 1] = [datetime.now() + time_offset] + [self.iexVar.get_price()]
+  
+  def getHistoricalData(self, StartDate, EndDate):
+    '''StartDate and EndDate are date objects'''
+
+    for date in daterange(StartDate, EndDate):
+      self.historicalData[date] = get_historical_intraday(self.ticker, date, output_format='pandas', token = IEX_TOKEN)
+
+
+aapl = Equity(name='Apple', ticker = 'AAPL') 
+
+start_date = date(2020, 5, 22)
+end_date = date(2020, 6, 2)
+
+aapl.getHistoricalData(start_date,end_date)
 
 
 
