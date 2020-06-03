d7e89ffcf0c53d62ac9119be80cbb9d1abf0210e
justinjiang1212 <jbjiang@hmc.edu>
2020-06-03 01:30:20 -0700

    more updates

--- hw/final_project/code/Data.py
+++ hw/final_project/code/Data.py
@@ -2,32 +2,38 @@
 
 from datetime import datetime, timedelta
 from iexfinance.stocks import Stock
+from iexfinance.iexdata import get_last
 import time as Time
 import matplotlib
 import matplotlib.pyplot as plt
 
 
 a = Stock("AAPL", token="sk_434806dae94a4ee69daa8375f33da6f5") #token is the secret API key for our account
+IEX_TOKEN='sk_434806dae94a4ee69daa8375f33da6f5'
 
 time_offset = timedelta(hours = 3)
 nyc_time = datetime.now() + time_offset
 
 intraday_prices = []
 time = []
-#counter = 0
+counter = 0
 
-#while counter < 5:
-while (nyc_time.hour > 9) and (nyc_time.minute > 30):
-  intraday_prices.append(a.get_price())
+while counter < 500:
+#while (nyc_time.hour > 9) and (nyc_time.minute > 30):
+  #intraday_prices.append(a.get_price())
+
   
-  time.append(str(nyc_time.hour) + str(":") + str(nyc_time.minute))
+  df = get_last(symbols="AAPL", output_format='pandas', token = IEX_TOKEN)
+  intraday_prices.append(df['price'])
 
-  Time.sleep(60)
+  time.append(str(nyc_time.minute) + str(":") + str(nyc_time.second))
+
+  Time.sleep(10)
   nyc_time = datetime.now() + time_offset
 
   if (nyc_time.hour > 14) and (nyc_time.minute > 30) and (nyc_time.second > 0):
     break
-  #counter +=1
+  counter +=1
 
 
 
